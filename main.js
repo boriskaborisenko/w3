6f52ebddd230d6dcf8d4c00c91814c7c1b21e288
MacBookPro2020 <boriskaborisenko@gmail.com>
2022-12-02 10:33:44 +0200

    a

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const x = moment().diff(moment([2022, 1, 24]), 'days') // x+Nth
-document.querySelector('#day').innerHTML = x+1+' DAY'
+
+const y = (x+1).toString()
+const last = y.charAt(y.length - 1);
+console.log(last,'LAST')
+
+let th = 'th'
+last === '1' ? th = 'st' : 'th'
+last === '2' ? th = 'nd' : 'th'
+last === '3' ? th = 'rd' : 'th'
+
+console.log(th,'TH')
+
+document.querySelector('#day').innerHTML = '<i>'+y+'</i><i class="th">'+th+'</i><br><x class="x">DAY</x>'
